0748c078e5701c437fd7f313451b6c23a5656621
Lee JY <iplab9914@gmail.com>
2024-12-18 18:29:31 +0900

    bootstrap template upload

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('html/index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    return render_template('html/login.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
