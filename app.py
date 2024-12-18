cd934dfba8bd3f8dd1a3c90aff7301853ba676af
Lee JY <iplab9914@gmail.com>
2024-12-18 15:40:29 +0900

    update .gitignore

--- app.py
+++ app.py
@@ -5,5 +5,8 @@ app = Flask(__name__)
 def index():
     return render_template('html/index.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
